713eea24731d46365573f689a9ff93e0be126b9e
Yongrok Oh <ohyr96@ajou.ac.kr>
2021-02-13 15:59:45 +0900

    Posts 의 Spring Data JPA 적용에 대한 Test 코드 작성

--- src/main/java/com/ohyr/example/springboot/domain/posts/Posts.java
+++ src/main/java/com/ohyr/example/springboot/domain/posts/Posts.java
@@ -30,7 +30,7 @@ public class Posts {
     @Builder
     public Posts(String title, String content, String author){
         this.title = title;
-        this.content = title;
+        this.content = content;
         this.author = author;
     }
 }
